29702dad77d42eade64137daeac2ce704db2f0cf
besantos1980-ui <besantos1980@gmail.com>
2026-05-21 15:07:56 -0300

    Change CSV encoding from WIN1252 to CP1252
    Updated the encoding from 'WIN1252' to 'CP1252' in the CSV read operation.

--- 01_extracao_cadop.py
+++ 01_extracao_cadop.py
@@ -4,13 +4,14 @@ def extrair_cadop():
     print("Iniciando extração do CADOP...")
     con = duckdb.connect('banco_ans.db')
     
+    # Mudamos o encoding de 'WIN1252' para 'CP1252'
     query = """
     CREATE OR REPLACE TABLE cadop AS 
     SELECT 
         CAST(Registro_ANS AS VARCHAR) AS REG_ANS,
         Nome_Fantasia,
         Modalidade
-    FROM read_csv_auto('https://dadosabertos.ans.gov.br/FTP/PDA/operadoras_de_plano_de_saude_ativas/Relatorio_cadop.csv', encoding='WIN1252')
+    FROM read_csv_auto('https://dadosabertos.ans.gov.br/FTP/PDA/operadoras_de_plano_de_saude_ativas/Relatorio_cadop.csv', encoding='CP1252')
     WHERE Modalidade != 'Administradora de Benefícios';
     """
     con.execute(query)
